d8f3bcf1aff13042f3545d0dd6309c67f77a99f1
cacevedo2011 <cristianacevedo2011@gmail.com>
2020-12-10 19:01:02 -0500

    All of the project working
    I needed to do this to make the code work

--- routes/api/pizza-routes.js
+++ routes/api/pizza-routes.js
@@ -1,12 +1,11 @@
 const router = require('express').Router();
-
 const {
-    getAllPizza,
-    getPizzaById,
-    createPizza,
-    updatePizza,
-    deletePizza
-  } = require('../../controllers/pizza-controller');
+  getAllPizza,
+  getPizzaById,
+  createPizza,
+  updatePizza,
+  deletePizza
+} = require('../../controllers/pizza-controller');
 
 // /api/pizzas
 router
@@ -20,18 +19,5 @@ router
   .get(getPizzaById)
   .put(updatePizza)
   .delete(deletePizza);
-
-// Set up GET all and POST at /api/pizzas
-router
-  .route('/')
-  .get()
-  .post();
-
-// Set up GET one, PUT, and DELETE at /api/pizzas/:id
-router
-  .route('/:id')
-  .get()
-  .put()
-  .delete();
 
 module.exports = router;
